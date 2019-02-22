4669f6c5f25f9456297ec8bd01464a3715b550cf
WarrenPitterson <warrenpitterson@gmail.com>
2019-02-22 22:38:18 +0000

    The one time aiden tried to teach me React

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
+const shopitems = () => (
+  "Hello World"
+);
 
 class App extends Component {
 
   render() {
-    return (
-      <div>Hello World</div>
-    );
-  }
+    return (<div>{shopitems}</div>)
+}
 }
 
 export default App;
---
+++ src/components/shop-items.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ShopItems extends Component {
+
+    render() {
+      return (
+        <div>Hello World</div>
+      );
+    }
+  }
+  
+  export default shop-items;
+  
